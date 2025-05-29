ec150162ef5d74a4b3600500d620e73133291725
ashleyvarghese <ashley.varghese8146@gmail.com>
2025-05-29 15:20:40 -0700

    Update contact form with new Formspree endpoint

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
     
     try {
       // Send to Formspree
-      const response = await fetch("https://formspree.io/f/xqkobqnv", {
+      const response = await fetch("https://formspree.io/f/mwpbekll", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
